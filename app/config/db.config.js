7c3ee0b0083e3999f47f7aa21849737c520279f0
tarnoma <p_jare@hotmail.com>
2023-06-08 16:25:55 +0700

    modify db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    HOST: "localhost",
-    USER : "root",
-    PASSWORD: "",
-    DB: "projectDB"
+    // HOST: "localhost",
+    // USER : "root",
+    // PASSWORD: "",
+    // DB: "projectDB"
+    HOST: "sql.freedb.tech",
+    USER : "freedb_electric",
+    PASSWORD: "t2*tVf92t&2AEZv",
+    DB: "freedb_electric_system"
 }
